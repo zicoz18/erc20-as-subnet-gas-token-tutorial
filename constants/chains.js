6150953e990fda0748b507b7cc5c96283db997d5
zicoz18 <zicoz18@ku.edu.tr>
2022-08-02 13:58:09 +0300

    Add working example of the bridge

---
+++ constants/chains.js
@@ -0,0 +1,11 @@
+module.exports = {
+	avax: {
+		chainId: 43113,
+		rpcUrl: "https://api.avax-test.network/ext/bc/C/rpc",
+	},
+	subnet: {
+		chainId: 676767,
+		rpcUrl:
+			"http://127.0.0.1:9650/ext/bc/2ZXBZh6am7dajLpaqYAifxevAbsrf5yCiL97N86uHif68Zz3sC/rpc",
+	},
+};
